5190221a0d86fe7d517749f2d2dab6f932359d5b
golgol <goldmangal90@gmail.com>
2021-03-24 14:35:46 +0200

    initial structure

---
+++ serchPS5.py
@@ -0,0 +1,12 @@
+class FindPS5:
+    def __init__(self):
+        pass
+    def websites_init(self):
+        pass
+    def search_in_website(self,website):
+        pass
+
+
+
+if __name__ == "__main__":
+    pass
